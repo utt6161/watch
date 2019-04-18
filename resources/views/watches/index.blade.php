6dbbeda447728d4f256ed92d3bf7c3b212d603f7
Ivan Khromov <khromov.ivan@gmail.com>
2019-04-18 09:51:42 +0500

    сделал отображение гридом

--- resources/views/watches/index.blade.php
+++ resources/views/watches/index.blade.php
@@ -18,35 +18,20 @@
 @section('content')
 <div class = "text-center" style = "padding-left:60px; padding-right:60px">
 	<h3 style = "color:white; padding-bottom:15px">Предложение ограничено!</h4>
-	<table style="width:100%" class="table">
-		<thead>
-			<th>Название</th>
-			<th>Цена</th>
-			<th>Изображение</th>
-			<th> </th>
-		</thead>
-		<tbody>
-			@foreach($watches as $w)
-			<form method="POST" action = "{{ route('cart.add') }}">
-				@csrf 
-				<input name="id" type="hidden" value="{{ $w->watchid }}">
-				<input name="name" type="hidden" value="{{ $w->name }}">
-				<input name="price" type="hidden" value="{{ $w->price }}">
-				<input name="image" type="hidden" value="{{ $w->image }}">
-				<tr>
-					<td>
-						<a href="{{ route('watches.view',['id'=>$w->id]) }}">
-							{{ $w->name }}
-						</a>
-					</td>
-					<td>{{ $w->price }}</td>
-					<td><img style="width:200px; height:auto;" src="{{ asset($w->image) }}"></td>
-					<td><button type="submit">В корзину</button></td>
-				</tr>
-			</form>
-				@endforeach
-			</tbody>
-  	</table>
+    <div class="container">
+        <div class="row">
+            @foreach($watches as $w)
+                <div class="col-3">
+                    <img src="{{ asset($w->image) }}" class="card-img-top" alt="{{ $w->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $w->name }}</h5>
+                        <p class="card-text lead">{{ $w->price }}</p>
+                        <a href="{{ route('watches.view',['id'=>$w->id]) }}" class="btn btn-primary">Подробнее</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 @stop
 
